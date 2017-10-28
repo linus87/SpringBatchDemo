d8bdb439faa9e3ecef698cec2893f81ef37a0d10
Linus <linus.yan87@gmail.com>
2017-10-28 23:41:02 +0800

    add configuration

--- springbatch3/src/main/java/com/linus/batch/springbatch3/App.java
+++ springbatch3/src/main/java/com/linus/batch/springbatch3/App.java
@@ -1,13 +1,21 @@
 package com.linus.batch.springbatch3;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linus.batch.springbatch3.configuration.BatchConfiguration;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@EnableBatchProcessing
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.exit(SpringApplication.exit(SpringApplication.run(
+				BatchConfiguration.class, args)));
+	}
 }
